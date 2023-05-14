42bc015fe960749c78d077e6c1cd3d992c899806
hassanahmhassan93 <hassanahmhassan93@gmail.com>
2023-05-14 08:23:04 +0300

    Add existing project files to Git

---
+++ src/test/java/com/photo/app/api/accountsmanagement/PhotoAppApiAccountsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo.app.api.accountsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
